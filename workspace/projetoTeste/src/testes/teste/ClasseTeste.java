fcb5391c8d460e9d32a7e6d06773f8f2ca8b956e
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2015-11-28 15:34:44 -0200

    gitignore recommenders

---
+++ workspace/projetoTeste/src/testes/teste/ClasseTeste.java
@@ -0,0 +1,18 @@
+package testes.teste;
+
+public class ClasseTeste<Teste> {
+
+	private Teste teste;
+
+	public ClasseTeste() {
+	}
+
+	public Teste getTeste() {
+		return this.teste;
+	}
+
+	public void setTeste(Teste teste) {
+		this.teste = teste;
+	}
+
+}
