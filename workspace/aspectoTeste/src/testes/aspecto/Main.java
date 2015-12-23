3b3b564525d77a53ea05dae89a0526b5df1f5b61
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2015-12-23 05:14:56 -0200

    tentativa aspect

--- workspace/aspectoTeste/src/testes/aspecto/Main.java
+++ workspace/aspectoTeste/src/testes/aspecto/Main.java
@@ -3,11 +3,10 @@ package testes.aspecto;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			@SuppressWarnings("unused")
 			Pessoa gerson = new Pessoa(null);
-			System.out.println(gerson.getNome());
-			gerson.setNome(null);
 		}catch(IllegalArgumentException erro) {
-			System.out.println(erro.getMessage());
+			erro.printStackTrace();
 		}
 	}
 }
